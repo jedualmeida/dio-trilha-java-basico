557ec012ebd6cc5cd1468fd5db0b29dbcba40160
jedualmeida <joseeduardoalmeida@live.com>
2023-03-16 21:25:19 -0300

    conclusao módulo operadores

--- src/edu/jedualmeida/operadores/Operadores.java
+++ src/edu/jedualmeida/operadores/Operadores.java
@@ -1,9 +1,11 @@
 package edu.jedualmeida.operadores;
 
+import java.util.FormatterClosedException;
+
 public class Operadores {
     public static void main (String [] args) {
 
-        //Exemplos de Operador de ATRIBUIÇÃO:
+        //Operador de ATRIBUIÇÃO:
 
         int idade = 32;
         double peso = 97.8;
@@ -11,9 +13,9 @@ public class Operadores {
         char sexo = 'M';
         boolean doadorOrgao = true;
 
+        System.out.println("-----------------------------------------------------------------------------------");
 
-
-        //Exemplos de Operadores ARITIMÉTICOS:
+        //Operadores ARITIMÉTICOS:
 
         double soma = 10.5 + 15.7;
         int subtracao = 113 - 25;
@@ -36,9 +38,9 @@ public class Operadores {
         concatenacao = "1" + (1 + 1 + 1);
         System.out.println(concatenacao);*/
 
+        System.out.println("-----------------------------------------------------------------------------------");
 
-
-        //Exemplos de Operadores UNÁRIOS:
+        //Operadores UNÁRIOS:
 
         int temperatura1 = +30;  //Para valores positivos
         int temperatura2 = -5;  //Para valores negativos e para negar um número ou expressão
@@ -53,6 +55,100 @@ public class Operadores {
         System.out.println(!casado);  //Nega o valor de uma expressão booleana. Nesse exemplo, resultado é true
         casado = !casado;
         System.out.println(casado);
+
+        System.out.println("-----------------------------------------------------------------------------------");
+
+        //Operadores TERNÁRIOS:
+
+        /* Exemplo de condicional utilizando estrutura If/Else:
+        int a, b;
+        String resposta = "";
+
+        a = 5;
+        b = 6;
+
+        if (a == b)
+            resposta = "Verdadeiro";
+        else
+            resposta = "Falso";
+        
+        System.out.println(resposta);
+        */
+
+        // Agora um exemplo de condicional utilizando operadores TERNÁRIOS:
+
+        int x, y;
+        x = 8;
+        y = 8;
+
+        String resposta1 = x == y ? "Verdadeiro" : "Falso";
+        System.out.println(resposta1);
+
+        int resposta2 = x == y ? 1 : 0;
+        System.out.println(resposta2);
+
+        System.out.println("-----------------------------------------------------------------------------------");
+
+        //Operadores RELACIONAIS:
+
+        int num1 = 1;
+        int num2 = 2;
+
+        boolean simNao = num1 == num2;  //Relação de IGUALDADE
+
+        System.out.println("numero Um é igual ao numero Dois? " + simNao);
+
+        simNao = num1 != num2;  //Relação de DIFERENÇA
+
+        System.out.println("numero Um é diferente do numero Dois? " + simNao);
+
+        simNao = num1 > num2;  //Relação de MAIOR QUE
+
+        System.out.println("numero Um é maior que o numero Dois? " + simNao);
+
+        if (num1 < num2){  //Relação de MENOR QUE
+            System.out.println("Nossa condição É Verdadeira");
+        }
+        else
+            System.out.println("Nossa condição NÃO É Verdadeira");
+
+        String nomeUm = "Eduardo";
+        String nomeDois = new String("Eduardo");
+
+        System.out.println(nomeUm.equals(nomeDois));  //.equals compara o conteúdo entre dois objetos, e é a forma mais recomendada para isso
+
+        System.out.println("-----------------------------------------------------------------------------------");
+
+        //Operadores LÓGICOS:
+
+        // && - E   || - OU
+
+        /*Tabela Verdade:
+        Verdadeiro E Verdadeiro = Verdadeiro
+        Verdadeiro E Falso = Falso
+        Falso E Verdadeiro = Falso
+        Falso E Falso = Falso
+
+        Verdadeiro OU Verdadeiro = Verdadeiro
+        Verdadeiro OU Falso = Verdadeiro
+        Falso OU Verdadeiro = Verdadeiro
+        Falso OU Falso = Falso
+        */
+
+        boolean condicao1 = true;
+        boolean condicao2 = false;
+
+        if (condicao1 && condicao2){
+            System.out.println("As duas condições são Verdadeiras");
+        }
+
+        if (condicao1 || condicao2){
+            System.out.println("Uma das condiçoes é Verdadeira");
+        }
+
+        if (condicao1 && (7 > 4)){
+            System.out.println("As duas condições são Verdadeiras");
+        }
 
     }
 
