2a9c2127e26403f574ab2a5045e3fbb8e531c7f0
jedualmeida <joseeduardoalmeida@live.com>
2023-03-18 20:28:47 -0300

    adicionado conteudo sobre java doc

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"edu.jedualmeida.terminaleargumentos","l":"Calculadora"},{"p":"edu.jedualmeida.terminaleargumentos","l":"Comentarios"}];updateSearchResults();
---
+++ src/edu/jedualmeida/javadoc/Comentarios.java
@@ -0,0 +1,26 @@
+package edu.jedualmeida.javadoc;
+
+public class Comentarios {
+    public static void main(String[] args) {
+
+        //Utilizado para fazer comentários no código em uma única linha.
+
+        /*Utilizado quando for
+        * necessário um comentário mais detalhado,
+        * que necessite de mais de uma linha.
+        */
+
+        /**
+        * Os dois asteríscos servem para identificar
+        * que pretende-se elaborar um comentário
+        * mas a nível de DOCUMENTAÇÃO.
+        */
+
+        /* IMPORTANTE!
+        * Utilizar comando abaixo via terminal para gerar a documentação do método e acesso HTML:
+        * javadoc -encoding UTF-8 -docencoding ISO-8859-1 -d ../docs src/edu/jedualmeida/javadoc/*.java
+        */
+
+    }
+    
+}
