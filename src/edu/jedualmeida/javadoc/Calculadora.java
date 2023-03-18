87bc87a598a01d975861e7c7e191ad2c2b213735
jedualmeida <joseeduardoalmeida@live.com>
2023-03-18 20:27:50 -0300

    adicionado conteudo sobre java doc

---
+++ src/edu/jedualmeida/javadoc/Calculadora.java
@@ -0,0 +1,25 @@
+package edu.jedualmeida.javadoc;
+/**
+* <h1>Calculadora</h1>
+* Uma Calculadora que realiza operações matemáticas entre números inteiros
+* <p>
+<b>Note:</b> Leia atentamente a documentação desta classe
+* para desfrutar integralmente dos recursos oferecidos pelo autor
+*
+* @author   José Eduardo Almeida
+* @version   1.0
+* @since   01/03/2023
+*/
+public class Calculadora {
+    /**
+    * Este método é utilizado para somar dois números inteiros
+    * @param numeroUm este é o primeiro parâmtero do método
+    * @param numeroDois este é o segundo parâmtero do método
+    * @return int o resultado deste método é a soma dois dois números
+    */
+    public int somar(int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois;
+
+    }
+    
+}
